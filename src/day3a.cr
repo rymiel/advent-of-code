bce9d79e4dc278c24585fe5fb0e31a6719dd11ae
Emilia Kond <emilia@rymiel.space>
2023-12-04 13:32:52 +0200

    optimize part 3

--- src/day3a.cr
+++ src/day3a.cr
@@ -2,17 +2,18 @@ DIRS = [{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1
 
 chars = File.read("txt/day3").strip
 map = chars.split("\n").map_with_index { |line, x| line.chars.map_with_index { |c, y| { {x, y}, c } } }.flatten.to_h
+keys = map.keys
 
-chars = chars.delete('\n')
+chars = chars.delete('\n').chars
 
-p chars.chars.map_with_index { |c, i|
+p chars.map_with_index { |c, i|
   next 0 if i > 0 && chars[i - 1].ascii_number?
 
-  endnum = chars[i..].chars.each_with_index.find(&.[0].ascii_number?.!).try &.[1]
+  endnum = chars[i..].each_with_index.find(&.[0].ascii_number?.!).try &.[1]
   next 0 if endnum.nil? || endnum == 0
 
   number = chars[i...(i + endnum)]
-  coords = map.keys[i...(i + endnum)]
+  coords = keys[i...(i + endnum)]
 
   has_symbol_neighbour = coords.any? { |(x, y)|
     DIRS.any? { |(dir_x, dir_y)|
@@ -21,6 +22,6 @@ p chars.chars.map_with_index { |c, i|
     }
   }
   next 0 unless has_symbol_neighbour
-  number.to_i
+  number.join.to_i
 }.sum
 
--- src/day3b.cr
+++ src/day3b.cr
@@ -2,25 +2,26 @@ DIRS = [{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1
 
 chars = File.read("txt/day3").strip
 map = chars.split("\n").map_with_index { |line, x| line.chars.map_with_index { |c, y| { {x, y}, c } } }.flatten.to_h
+keys = map.keys
 
-chars = chars.delete('\n')
+chars = chars.delete('\n').chars
 
 gears = map.select { |_, i| i == '*' }.map { |i, _| {i, [] of Int32} }.to_h
 
-chars.chars.each_with_index { |c, i|
+chars.each_with_index { |c, i|
   next if i > 0 && chars[i - 1].ascii_number?
 
-  endnum = chars[i..].chars.each_with_index.find(&.[0].ascii_number?.!).try &.[1]
+  endnum = chars[i..].each_with_index.find(&.[0].ascii_number?.!).try &.[1]
   next if endnum.nil? || endnum == 0
 
   number = chars[i...(i + endnum)]
-  coords = map.keys[i...(i + endnum)]
+  coords = keys[i...(i + endnum)]
 
   # I don't know if a number can be a part of multiple "gears" but I'm gonna write this assuming it can
   gears.keys.select { |(x, y)|
     DIRS.any? { |(dir_x, dir_y)| {x + dir_x, y + dir_y}.in? coords }
   }.each { |gear|
-    gears[gear] << number.to_i
+    gears[gear] << number.join.to_i
   }
 }
 
