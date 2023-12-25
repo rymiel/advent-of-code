48908f0cf3ef09604ed0500b5ad85112b245c3a9
Emilia Kond <emilia@rymiel.space>
2023-12-25 11:02:35 +0200

    Slight optimization for day 25

--- 2023/src/day25a.cr
+++ 2023/src/day25a.cr
@@ -1,30 +1,43 @@
-alias Wire = {String, String}
-WIRES = Array(Wire).new
-NODES = Set(String).new
+
+STRING_WIRES = Array({String, String}).new
+STRING_NODES = Set(String).new
 
 File.read_lines("txt/day25").each do |line|
   src, rest = line.split(": ")
   dsts = rest.split(" ")
   dsts.each do |dst|
-    WIRES << {src, dst}
-    NODES << src << dst
+    STRING_WIRES << {src, dst}
+    STRING_NODES << src << dst
   end
+end
+
+INDEX = Array(String).new
+INDEX.concat STRING_NODES
+
+alias Wire = {Int32, Int32}
+WIRES = Array(Wire).new
+NODES = Set(Int32).new
+STRING_WIRES.each do |(a, b)|
+  WIRES << {INDEX.index!(a), INDEX.index!(b)}
+end
+STRING_NODES.each do |i|
+  NODES << INDEX.index!(i)
 end
 
 class Karger
   getter wires : Array(Wire) = Array(Wire).new
-  getter nodes : Set(String) = Set(String).new
+  getter nodes : Set(Int32) = Set(Int32).new
   getter counter = 0
-  getter contractions = Hash(String, Int32).new
+  getter contractions = Hash(Int32, Int32).new
 
   def reset
     @wires = @initial_wires.dup
     @nodes = @initial_nodes.dup
-    @count = 0
+    @counter = @nodes.size + 1
     @contractions.clear
   end
 
-  def initialize(@initial_wires : Array(Wire), @initial_nodes : Set(String))
+  def initialize(@initial_wires : Array(Wire), @initial_nodes : Set(Int32))
   end
 
   def run
@@ -44,19 +57,16 @@ class Karger
 
   def next_name
     @counter += 1
-    "A" + @counter.to_s(base: 36)
+    @counter
   end
 
   def contract
-    edge_num = Random.rand(0...wires.size)
-    edge = wires[edge_num]
+    edge = wires[Random.rand(0...wires.size)]
     name = next_name
     nodes.delete edge[0]
     nodes.delete edge[1]
 
-    contractions[name] = (contractions[edge[0]]? || 1) + (contractions[edge[1]]? || 1)
-    contractions.delete edge[0]
-    contractions.delete edge[1]
+    contractions[name] = (contractions.delete(edge[0]) || 1) + (contractions.delete(edge[1]) || 1)
 
     wires.reject! { |i| i == edge || i.reverse == edge }
     wires.map! { |i|
