9e79da0b18c99e23b28264d744572e760aa3bcaf
Emilia Kond <emilia@rymiel.space>
2024-12-26 11:05:38 +0200

    Slightly simplify 2024 day 25

--- 2024/day25.ml
+++ 2024/day25.ml
@@ -1,9 +1,8 @@
 open Aoclib.Util
 
-let rec read_keys_locks (lines : string list) =
-  match partition_at "" lines with
-  | [], _ -> []
-  | head :: tail, rest ->
+let partition_locks_keys = function
+  | [] -> failwith "empty"
+  | head :: tail ->
       let is_lock = head = "#####" in
       let columns =
         List.to_seq tail |> Seq.take 5 |> Seq.map String.to_seq |> Seq.transpose
@@ -11,12 +10,12 @@ let rec read_keys_locks (lines : string list) =
         |> List.of_seq
       in
       (if is_lock then Either.left else Either.right) columns
-      :: read_keys_locks rest
 
 let fits = List.for_all2 (fun a b -> a + b <= 5)
 
 let day25a i =
   let locks, keys =
-    read_keys_locks (In_channel.input_lines i) |> List.partition_map Fun.id
+    i |> In_channel.input_lines |> partition_all ""
+    |> List.partition_map partition_locks_keys
   in
   List.map (fun key -> count_matches (fits key) locks) keys |> sum
