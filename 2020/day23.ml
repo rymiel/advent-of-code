d1633e80691878b516e4c01dea290ff544c663a0
Emilia Kond <emilia@rymiel.space>
2025-01-02 06:45:44 +0200

    2020 day 23 part 2

--- 2020/day23.ml
+++ 2020/day23.ml
@@ -1,66 +1,74 @@
 open Aoclib.Util
 
-module CircularArray = struct
-  let of_list = Array.of_list
-
-  let to_string arr =
-    Array.map string_of_int arr
-    |> Array.to_list |> String.concat " " |> Printf.sprintf "[%s]"
-
-  let get arr n = Array.get arr (n mod Array.length arr)
-  let set arr n = Array.set arr (n mod Array.length arr)
-
-  let blit src src_pos dst dst_pos len =
-    for i = 0 to len - 1 do
-      set dst (dst_pos + i) (get src (src_pos + i))
-    done
-
-  let pick_up arr n =
-    let buf = Array.make 3 0 in
-    blit arr (n + 1) buf 0 3;
-    buf
-
-  let destination_label arr n picked_up =
-    let rec aux n =
-      if n = 0 then aux 9 else if Array.mem n picked_up then aux (n - 1) else n
+module NewCircularArray = struct
+  let of_list list =
+    let max = List.length list in
+    let arr = Array.make max (-1) in
+    let rec aux = function
+      | a :: (b :: _ as n) ->
+          arr.(a - 1) <- b;
+          aux n
+      | a :: [] -> arr.(a - 1) <- List.hd list
+      | [] -> ()
     in
-    aux (get arr n - 1)
-
-  let find_index arr n e =
-    let t = Array.length arr in
-    let rec loop i =
-      if i = t then failwith "not found"
-      else if get arr (n + i) = e then n + i
-      else loop (succ i)
-    in
-    loop 0
-
-  let do_move arr n =
-    let picked_up = pick_up arr n in
-    let dest_label = destination_label arr n picked_up in
-    let dest_idx = find_index arr n dest_label in
-    let move_n = dest_idx - (n + 3) in
-    blit arr (n + 4) arr (n + 1) move_n;
-    blit picked_up 0 arr (n + 1 + move_n) 3;
+    aux list;
     arr
 
-  let order arr =
-    let one = find_index arr 0 1 in
-    Seq.ints 1 |> Seq.take 8
-    |> Seq.map (fun i -> get arr (i + one))
+  let next arr n = arr.(n - 1)
+  let set_after arr k v = arr.(k - 1) <- v
+
+  let pick_up arr n =
+    let p1 = next arr n in
+    let p2 = next arr p1 in
+    let p3 = next arr p2 in
+    (p1, p2, p3)
+
+  let destination arr n (p1, p2, p3) =
+    let rec aux n =
+      if n = 0 then aux (Array.length arr)
+      else if n = p1 || n = p2 || n = p3 then aux (n - 1)
+      else n
+    in
+    aux (n - 1)
+
+  let do_move (arr, current) =
+    let p1, p2, p3 = pick_up arr current in
+    let dest = destination arr current (p1, p2, p3) in
+    let after_dest = next arr dest in
+    let after_p3 = next arr p3 in
+    set_after arr current after_p3;
+    set_after arr dest p1;
+    set_after arr p3 after_dest;
+    (arr, next arr current)
+
+  let order_part1 arr =
+    Seq.iterate (next arr) 1
+    |> Seq.drop 1
+    |> Seq.take (Array.length arr - 1)
     |> Seq.map string_of_int |> List.of_seq |> String.concat "" |> int_of_string
 
-  let iterate (arr, n) = (do_move arr n, n + 1)
+  let product_part2 arr =
+    let a = next arr 1 in
+    let b = next arr a in
+    a * b
 end
 
+let parse i =
+  In_channel.input_all i |> String.trim |> chars |> List.map digit_of_char
+
 let day23a i =
-  let arr =
-    In_channel.input_all i |> String.trim |> chars |> List.map digit_of_char
-    |> CircularArray.of_list
-  in
-  Seq.iterate CircularArray.iterate (arr, 0)
-  |> Seq.drop 100 |> head |> fst |> CircularArray.order
+  let input = parse i in
+  let narr = NewCircularArray.of_list input in
+  Seq.iterate NewCircularArray.do_move (narr, List.hd input)
+  |> Seq.drop 100 |> head |> fst |> NewCircularArray.order_part1
 
 let day23b i =
-  ignore i;
-  failwith "todo"
+  let input = parse i in
+  let rest =
+    Seq.ints (ListExt.max input + 1)
+    |> Seq.take_while (fun i -> i <= 1_000_000)
+    |> List.of_seq
+  in
+  let narr = NewCircularArray.of_list (input @ rest) in
+  Seq.iterate NewCircularArray.do_move (narr, List.hd input)
+  |> Seq.drop 10_000_000 |> head |> fst |> NewCircularArray.product_part2
