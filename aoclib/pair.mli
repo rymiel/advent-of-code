3bc5e412b7c2aae8c3a1cf5f15908649761f2453
Emilia Kond <emilia@rymiel.space>
2025-01-13 14:33:02 +0200

    Don't need an mli for this one

--- aoclib/pair.mli
+++
@@ -1,7 +0,0 @@
-type ('a, 'b) t = 'a * 'b
-val map : ('a -> 'b) -> 'a * 'a -> 'b * 'b
-val apply : ('a -> 'b -> 'c) -> 'a * 'b -> 'c
-val flip : 'a * 'b -> 'b * 'a
-val mem : 'a -> 'a * 'a -> bool
-val to_seq : 'a * 'a -> 'a Seq.t
-val exists : ('a -> bool) -> 'a * 'a -> bool
